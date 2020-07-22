fae98cb919b23b7ac4afca9825788ffa32fcb267
hvenugopalan <hkrishnan1995@gmail.com>
2020-07-22 12:19:14 -0500

    Changing parameter from account num to username

--- CreatePortfolio_SP.sql
+++ CreatePortfolio_SP.sql
@@ -1,11 +1,17 @@
 DELIMITER $$
 CREATE PROCEDURE CreatePortfolio(
 IN type VARCHAR(30),
-IN account_number BIGINT
+IN usrname VARCHAR(50)
 )
 BEGIN
-	INSERT INTO portfolio(type, account_number) VALUES (type, account_number);
-    SELECT max(pid) as pid FROM portfolio;
+	DECLARE act_num BIGINT;
+    SELECT account_number INTO act_num FROM investor WHERE username = usrname;
+    IF act_num IS NULL THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'A user with the given username does not exist.', MYSQL_ERRNO = 1001;
+	ELSE
+		INSERT INTO portfolio(type, account_number) VALUES (type, act_num);
+		SELECT max(pid) as pid FROM portfolio;
+	END IF;
 END $$
 DELIMITER ;
 
