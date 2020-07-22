fecb0eef58af020a8fa36bd09af567657d51c4ad
hvenugopalan <hkrishnan1995@gmail.com>
2020-07-22 01:40:30 -0500

    Fixing error in GetUserInfo_SP

--- GetUserInfo_SP.sql
+++ GetUserInfo_SP.sql
@@ -2,14 +2,18 @@ DELIMITER $$
 CREATE PROCEDURE GetUserInfo(
 IN username VARCHAR(50))
 BEGIN
-		SELECT p.pid, p.type, c.amount, i.first_name, i.last_name 
+		SELECT p.pid, p.type, IFNULL(c.amount,0), i.first_name, i.last_name 
         FROM portfolio p 
-        INNER JOIN contains c ON p.pid = c.pid
-        INNER JOIN investor i on i.account_number = p.account_number
+		INNER JOIN investor i on i.account_number = p.account_number
+        LEFT JOIN contains c ON p.pid = c.pid
+       
         WHERE i.username = username;
 END $$
 DELIMITER ;
 
+-- select * from investor where account_number = 123456
+-- select * from portfolio where account_number = 123456
+-- select * from pid 
 -- call GetUserInfo('admin')
 
 -- drop procedure GetUserInfo
